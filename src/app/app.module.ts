32fab0978ab228cda302e72ed2d3f55c1f336ac3
Carolina Stomboli <carostomboli@hotmail.com>
2020-08-30 22:10:45 -0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ComponenteComponent } from './componente/componente.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
