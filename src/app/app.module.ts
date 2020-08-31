5cd39d5a26d63b582869f22d6798029dcd901bb5
Carolina Stomboli <carostomboli@hotmail.com>
2020-08-30 21:40:47 -0300

    oke

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent
   ],
   imports: [
     BrowserModule
